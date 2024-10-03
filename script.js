2e3fa8ebc69bcdddfb357a7b05044114ed8129ef
unknown <bgsdewoo20@gmail.com>
2024-10-03 14:37:05 +0700

    Apa Itu Json

---
+++ script.js
@@ -0,0 +1,35 @@
+// const jsonString = `{
+//     "ticker": {
+//       "base": "BTC",
+//       "target": "IDR",
+//       "price": "345001000.00000000",
+//       "volume": "28.02287745",
+//       "change": "-430000.00000000"
+//     },
+//     "timestamp": 1674472505,
+//     "success": true,
+//     "error": ""
+//   }`;
+
+// //convert JSON menjadi Objek Javascript
+// const res = JSON.parse(jsonString);
+// console.log(res.ticker.base); // Output: "BTC"
+// console.log(res.ticker.price); // Output: "harga"
+
+//versi dari JS menjadi JSON
+// const data = {
+//   ticker: {
+//     base: "BTC",
+//     target: "IDR",
+//     price: "345001000.00000000",
+//     volume: "28.02287745",
+//     change: "-430000.00000000",
+//   },
+//   timestamp: 1674472505,
+//   success: true,
+//   error: "",
+// };
+
+// // Mengubah objek JS menjadi string JSON
+// const jsonString = JSON.stringify(data);
+// console.log(jsonString);
