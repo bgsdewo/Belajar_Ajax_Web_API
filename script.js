05cfa5c924ac8c1a9c788343420b15d1fc42c043
unknown <bgsdewoo20@gmail.com>
2024-10-04 09:28:02 +0700

    Request Dengan Fetch

--- script.js
+++ script.js
@@ -52,3 +52,46 @@
 
 // req.open("GET", "https://swapi.dev/api/people/1/");
 // req.send();
+
+//topic Melakukan Request Dengan Fetch
+// fetch("https://swapi.dev/api/people/1")
+//   .then((res) => {
+//     if (!res.ok) {
+//       throw Error("Could not fetch the data for that resource");
+//     }
+//     // console.log(res);
+//     return res.json();
+//   })
+//   .then((data) => {
+//     console.log("json 1", data);
+//     return fetch("https://swapi.dev/api/people/2");
+//   })
+//   .then((res) => {
+//     console.log("request 2");
+//     if (!res.ok) {
+//       throw Error("Could not fetch the data for that resource");
+//     }
+//     return res.json();
+//   })
+//   .then((data) => {
+//     console.log(data);
+//   })
+//   .catch((err) => {
+//     console.log("error", err);
+//   });
+
+//ada cara lagi
+// const loadPeople = async () => {
+//   try {
+//     const res = await fetch("https://swapi.dev/api/peoplasjkdge/1");
+//     const data = await res.json();
+//     console.log(data);
+//     const res2 = await fetch("https://swapi.dev/api/people/2");
+//     const data2 = await res2.json();
+//     console.log(data2);
+//   } catch (err) {
+//     console.log("error", err);
+//   }
+// };
+
+// loadPeople();
