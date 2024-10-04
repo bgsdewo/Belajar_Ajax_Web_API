b49a280eecddc3524acf20efc51affadd07f7994
unknown <bgsdewoo20@gmail.com>
2024-10-04 15:49:32 +0700

    Melakukan Request Dengan Library Axios

--- script.js
+++ script.js
@@ -95,3 +95,48 @@
 // };
 
 // loadPeople();
+
+//Topic Melakukan Request Dengan Library Axios
+// axios
+// 	.get('https://swapi.dev/api/people123/1')
+// 	.then((res) => {
+// 		console.log(res.data);
+// 	})
+// 	.catch((err) => {
+// 		console.log(err);
+// 		alert(err.message);
+// 	});
+
+// axios
+// 	.get('https://swapi.dev/api/people123/1')
+// 	.then((res) => {
+// 		console.log(res.data);
+// 	})
+// 	.catch((err) => {
+// 		console.log(err);
+// 		alert(err.message);
+// 	});
+
+// axios
+//   .get("https://swapi.dev/api/people/1")
+//   .then((res) => {
+//     console.log(res.data);
+//   })
+//   .catch((err) => {
+//     console.log(err);
+//     alert(err.message);
+//   });
+
+// const getPeople = async (id) => {
+//   try {
+//     const res = await axios.get(`https://swapi.dev/api/people/${id}`);
+//     console.log(res.data);
+//   } catch (error) {
+//     console.log(error);
+//     console.log(error.message);
+//     console.log(error.response.status);
+//     console.log(error.response.data);
+//   }
+// };
+
+// getPeople(131232131312321321122);
